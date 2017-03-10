04bc1f714cfa8063e98c7da94237a234a087b7d3
Michael Fryer <mfryer@cs.mtech.edu>
2017-03-09 18:30:29 -0700

    fixed some stuff in the creation script

--- acm_Database_Create.sql
+++ acm_Database_Create.sql
@@ -1,3 +1,30 @@
+DELETE FROM Event_Files;
+DROP TABLE IF EXISTS Event_Files;
+
+DELETE FROM Files;
+DROP TABLE IF EXISTS Files;
+
+DELETE FROM Events;
+DROP TABLE IF EXISTS Events;
+
+DELETE FROM Debtor_Fees;
+DROP TABLE IF EXISTS Debtor_Fees;
+
+DELETE FROM Fees;
+DROP TABLE IF EXISTS Fees;
+
+DELETE FROM Users;
+DROP TABLE IF EXISTS Users;
+
+DELETE FROM Event_Type;
+DROP TABLE IF EXISTS Event_Type;
+
+DELETE FROM User_Type;
+DROP TABLE IF EXISTS User_Type;
+
+DELETE FROM Passwords;
+DROP TABLE IF EXISTS Passwords;
+
 CREATE TABLE Passwords (
   password_id int NOT NULL AUTO_INCREMENT,
   password varchar(255) NOT NULL,
@@ -6,14 +33,14 @@ CREATE TABLE Passwords (
 );
 
 CREATE TABLE User_Type (
-  user_type_id int NOT NULL AUTO_INCREMENT,
+  user_type_id tinyint NOT NULL AUTO_INCREMENT,
   name varchar(20) NOT NULL,
   description varchar(50),
   PRIMARY KEY(user_type_id)
 );
 
 CREATE TABLE Event_Type (
-  event_type_id int NOT NULL AUTO_INCREMENT,
+  event_type_id tinyint NOT NULL AUTO_INCREMENT,
   name varchar(20) NOT NULL,
   description varchar(50),
   PRIMARY KEY(event_type_id)
@@ -22,47 +49,13 @@ CREATE TABLE Event_Type (
 CREATE TABLE Users (
   user_id int NOT NULL AUTO_INCREMENT,
   password_id int NOT NULL,
-  user_type int NOT NULL,
+  user_type tinyint NOT NULL,
   fName varchar(20) NOT NULL,
   lName varchar(20) NOT NULL,
   email varchar(30) NOT NULL,
   FOREIGN KEY(password_id) REFERENCES Passwords(password_id),
   FOREIGN KEY(user_type) REFERENCES User_Type(user_type_id),
   PRIMARY KEY(user_id)
-);
-
-CREATE TABLE Files (
-  file_id int NOT NULL AUTO_INCREMENT,
-  uploader int NOT NULL,
-  audience int NOT NULL,
-  fileName varchar(30) NOT NULL,
-  description varchar(255),
-  FOREIGN KEY(uploader) REFERENCES Users(user_id),
-  FOREIGN KEY(audience) REFERENCES User_Type(user_type_id),
-  PRIMARY KEY(file_id)
-);
-
-CREATE TABLE Events (
-  event_id int NOT NULL AUTO_INCREMENT,
-  coordinator int NOT NULL,
-  EventType int NOT NULL,
-  name varchar(30) NOT NULL,
-  additionalInfo text,
-  location varchar(50),
-  time timestamp NOT NULL,
-  attendance int NOT NULL,
-  FOREIGN KEY(coordinator) REFERENCES Users(user_id),
-  FOREIGN KEY(eventType) REFERENCES Event_Type(event_type_id),
-  PRIMARY KEY(event_id)
-);
-
-CREATE TABLE Event_Files (
-  event_id int NOT NULL,
-  file_id int NOT NULL,
-  additonalInfo varchar(50),
-  FOREIGN KEY(event_id) REFERENCES Events(event_id),
-  FOREIGN KEY(file_id) REFERENCES Files(file_id),
-  PRIMARY KEY(event_id, file_id)
 );
 
 CREATE TABLE Fees (
@@ -80,4 +73,38 @@ CREATE TABLE Debtor_Fees (
   FOREIGN KEY(debtor_id) REFERENCES Users(user_id),
   FOREIGN KEY(fee_id) REFERENCES Fees(fee_id),
   PRIMARY KEY(debtor_id, fee_id)
+);
+
+CREATE TABLE Events (
+  event_id int NOT NULL AUTO_INCREMENT,
+  coordinator int NOT NULL,
+  EventType tinyint NOT NULL,
+  name varchar(30) NOT NULL,
+  additionalInfo text,
+  location varchar(50),
+  event_time timestamp NOT NULL,
+  attendance int NOT NULL,
+  FOREIGN KEY(coordinator) REFERENCES Users(user_id),
+  FOREIGN KEY(eventType) REFERENCES Event_Type(event_type_id),
+  PRIMARY KEY(event_id)
+);
+
+CREATE TABLE Files (
+  file_id int NOT NULL AUTO_INCREMENT,
+  uploader int NOT NULL,
+  audience tinyint NOT NULL,
+  fileName varchar(30) NOT NULL,
+  description varchar(255),
+  FOREIGN KEY(uploader) REFERENCES Users(user_id),
+  FOREIGN KEY(audience) REFERENCES User_Type(user_type_id),
+  PRIMARY KEY(file_id)
+);
+
+CREATE TABLE Event_Files (
+  event_id int NOT NULL,
+  file_id int NOT NULL,
+  additonalInfo varchar(50),
+  FOREIGN KEY(event_id) REFERENCES Events(event_id),
+  FOREIGN KEY(file_id) REFERENCES Files(file_id),
+  PRIMARY KEY(event_id, file_id)
 );
