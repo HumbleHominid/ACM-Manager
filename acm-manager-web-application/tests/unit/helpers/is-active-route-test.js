87436d68258500bded44d66728c8fc8c7d8d2e6a
HumbleHominid <michael.d.fryer@gmail.com>
2017-03-30 20:12:23 -0600

    fixed unit test errors

--- acm-manager-web-application/tests/unit/helpers/is-active-route-test.js
+++ acm-manager-web-application/tests/unit/helpers/is-active-route-test.js
@@ -5,8 +5,17 @@ import { module, test } from 'qunit';
 module('Unit | Helper | is active route');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = isActiveRoute([42]);
+test('apple apple', function(assert) {
+  let result = isActiveRoute(['apple', 'apple']);
   assert.ok(result);
 });
 
+test('Apple apple', function(assert) {
+  let result = isActiveRoute(['Apple', 'apple']);
+  assert.ok(!result);
+});
+
+test('1 1', function(assert) {
+  let result = isActiveRoute([1, 1]);
+  assert.ok(result);
+});
