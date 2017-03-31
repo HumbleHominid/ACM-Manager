dce5f4055dac0c00296e9d965d829462d808365d
Michael Fryer <michael.d.fryer@gmail.com>
2017-03-30 18:08:32 -0600

    navbar modifications

--- acm-manager-web-application/app/routes/application.js
+++ acm-manager-web-application/app/routes/application.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {thing: 'butts'};
+    return {
+      fName: 'Michael',
+      lName: 'Fryer',
+    };
   }
 });
