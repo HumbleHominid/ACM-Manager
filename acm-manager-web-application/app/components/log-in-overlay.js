621bf17c2bfb720820a8d5e7b64bd0f7255d24e8
Michael Fryer <michael.d.fryer@gmail.com>
2017-04-01 14:36:25 -0600

    commiting thing

--- acm-manager-web-application/app/components/log-in-overlay.js
+++ acm-manager-web-application/app/components/log-in-overlay.js
@@ -1,4 +1,9 @@
 import AbstractOverlay from './abstract-overlay';
 
 export default AbstractOverlay.extend({
+  actions: {
+    onCreateAccountButtonPress() {
+      this.get('creatrAccountCallback')();
+    }
+  }
 });
