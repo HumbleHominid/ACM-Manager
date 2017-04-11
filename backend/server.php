f9044db97830bdb7fb46c585840503e72a2d957e
Trevor Brooks <tbrooks@mtech.edu>
2017-04-10 20:00:16 -0600

    Base for rest

---
+++ backend/server.php
@@ -0,0 +1,224 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+class Server{
+
+  private $relAddress = '/~tbrooks/ACM-Manager/backend/';
+
+  //Main handler method
+  public function serve(){
+    //Get the URL info for REST
+    $URI = str_replace($this->relAddress, '', $_SERVER['REQUEST_URI']);
+    
+    //Get the method of the request.
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    //Explode the trimmed URI   
+    $explArr = explode('/', $URI);
+    //Get the object and id of the request
+    $object = $explArr[0];
+    $id = '';
+    if(count($explArr) >= 2){
+      $id = $explArr[1];
+    }
+    
+    switch($object){
+      case 'events':
+        events($method, $id); 
+        break;
+      case 'members':
+        members($method, $id);
+        break;
+      case 'login':
+        login($method, $id);
+        break;
+      case 'fees':
+        fees($method, $id);
+        break;
+      case 'files':
+        files($method, $id);
+        break;
+      case 'officers':
+        officers($method, $id);
+        break;
+      default:
+        header('HTTP/1.1 400 Bad Request');
+        break;
+    }
+
+  }
+
+  private function events($method, $id){
+    $item = NULL;
+    switch($method){
+      case 'GET':
+        
+        break;
+      case 'PUT':
+        if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+        }
+        break;
+      case 'POST':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'DELETE':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+    }
+    response($item);
+  }
+ 
+  private function members($method, $id){
+    switch($method){
+      case 'GET':
+        break;
+      case 'PUT':
+        if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+        }
+        break;
+      case 'POST':
+        if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+        break;
+      case 'DELETE':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+    } 
+  }
+
+  private function login($method, $id){
+    switch($method){
+      case 'POST':
+        break;  
+    } 
+  }
+
+  private function fees($method, $id){
+    switch($method){
+      case 'GET':
+        break;
+      case 'PUT':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'POST':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'DELETE':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+    } 
+  }
+  
+  private function files($method, $id){
+    switch($method){
+      case 'GET':
+        break;
+      case 'PUT':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'POST':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'DELETE':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+    } 
+  }
+  
+  private function officers($method, $id){
+    header('HTTP/1.1 200 OK');
+    echo "asdf"; 
+    switch($method){
+      case 'GET':
+        break;
+      case 'PUT':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'POST':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+      case 'DELETE':
+         if($id === ''){
+          header('HTTP/1.1 400 Bad Request');
+          exit();
+         }
+
+        break;
+    } 
+  } 
+  
+  
+  //Encodes the JSON response. If it is not valid, sends error header and ends.
+  private function response($builtArr){
+    $json = json_encode($builtArr);
+
+    if($json === FALSE){
+      header('HTTP/1.1 500 Internal Server Error');
+      exit();
+    }else{
+      header('Content-Type: application/json');
+      return $json;
+    }
+    return FALSE;
+  }
+}
+
+$server = new Server;
+$server->serve();
+
+
+?>
+
