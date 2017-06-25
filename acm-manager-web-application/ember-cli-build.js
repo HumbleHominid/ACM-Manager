5746cf7df03b053ab751eaaeb612232cfda8b743
Michael Fryer <michael.d.fryer@gmail.com>
2017-06-25 01:53:26 -0600

    Properly imported bootstrap this time

--- acm-manager-web-application/ember-cli-build.js
+++ acm-manager-web-application/ember-cli-build.js
@@ -1,14 +1,15 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var bootstrap = require('bootstrap');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': true
+    }
   });
-  
-  app.import(bootstrap);
   
   return app.toTree();
 };
