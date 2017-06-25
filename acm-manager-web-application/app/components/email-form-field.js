835712b9edbbc03c00e660dd6ea7f418c5262877
Michael Fryer <michael.d.fryer@gmail.com>
2017-06-25 01:43:34 -0600

    Changed all overlays to use bootstrap modals instead. Bootstrap properly imported in ember-cli-build. Removed content-area as it was deprecated

--- acm-manager-web-application/app/components/email-form-field.js
+++ acm-manager-web-application/app/components/email-form-field.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ["form-group", "has-feedback"],
+  placeholder: "Email",
+  idPrefix: "generic",
+  value: "",
+  
+  didRender() {
+    let el = this.$("#" + this.get('idPrefix') + "-email")[0];
+    let div = Ember.$("#" + this.elementId);
+    let span = this.$("#" + this.get('idPrefix') + "-email_span");
+    
+    if (el.validity.valid) {
+      div.removeClass("has-warning has-error").addClass("has-success");
+      span.removeClass("glyphicon-warning-sign glyphicon-remove").addClass("glyphicon-ok");
+    }//if
+    else if (!el.value && !el.validity.valid) {
+      div.removeClass("has-error has-success").addClass("has-warning");
+      span.removeClass("glyphicon-remove glyphicon-ok").addClass("glyphicon-warning-sign");
+    }//else if
+    else if (!el.validity.valid) {
+      div.removeClass("has-warning has-success").addClass("has-error");
+      span.removeClass("has-warning has-success").addClass("has-error");
+    }//else if
+  }
 });
--- acm-manager-web-application/app/components/nav-bar.js
+++ acm-manager-web-application/app/components/nav-bar.js
@@ -9,73 +9,16 @@ export default Ember.Component.extend({
   displayResetPassword: false,
 
   user: {
-    fName: '',
-    lName: '',
+    fName: 'Poop',
+    lName: 'Guy',
     email: '',
     userID: '',
-    user_type: 0,
-    jwt: null
+    user_type: 2,
+    jwt: "ioansthion"
   },
   actions: {
-    showLogIn() {
-      this.send('hideOverlays');
-
-      this.set('displayLogIn', true);
-
-      this.sendAction('darkenBackground');
-    },
-    showContactUs() {
-      this.send('hideOverlays');
-
-      this.set('displayContactUs', true);
-
-      this.sendAction('darkenBackground');
-    },
-    showAddUser() {
-      this.send('hideOverlays');
-
-      this.set('displayAddUser', true);
-
-      this.sendAction('darkenBackground');
-    },
-    showRemoveUser() {
-      this.send('hideOverlays');
-
-      this.set('displayRemoveUser', true);
-
-      this.sendAction('darkenBackground');
-    },
-    showCreateAccount() {
-      this.send('hideOverlays');
-
-      this.set('displayCreateAccount', true);
-
-      this.sendAction('darkenBackground');
-    },
-    showResetPassword() {
-      this.send('hideOverlays');
-
-      this.set('displayResetPassword', true);
-
-      this.sendAction('darkenBackground');
-    },
-    hideOverlays() {
-      this.set('displayCreateAccount', false);
-      this.set('displayRemoveUser', false);
-      this.set('displayLogIn', false);
-      this.set('displayContactUs', false);
-      this.set('displayAddUser', false);
-      this.set('displayResetPassword', false);
-    },
-    closeOverlay() {
-      this.send('hideOverlays');
-
-      this.sendAction('brightenBackground');
-    },
     logIn(params) {
       this.set('user', params);
-
-      this.send('closeOverlay');
     },
     logOut() {
       let user = {
