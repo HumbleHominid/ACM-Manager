a5e8501a83cae92472dc551dca55d8685e2178de
Michael Fryer <michael.d.fryer@gmail.com>
2017-06-28 18:41:53 -0600

    fixed account name getting put on a sepertate line on initial render

--- acm-manager-web-application/app/components/event-side-bar.js
+++ acm-manager-web-application/app/components/event-side-bar.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
-  didRender() {
-    let el = this.$("#upcoming-events");
-    
-    let eventsHeight = el.outerHeight(true) - parseInt(el.css("marginBottom"));
-    let panelBodyHeight = this.$(".panel-body").outerHeight(true);
-    let panelHeadingHeight = this.$(".panel-heading").outerHeight(true);
-    
-
-    this.$(".upcoming-events-list").css("height", this.get('eventsMaxHeight'));
-  }
 });
--- acm-manager-web-application/app/components/generic-modal.js
+++ acm-manager-web-application/app/components/generic-modal.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  notify: Ember.inject.service(),
+  notify: Ember.inject.service()
 });
