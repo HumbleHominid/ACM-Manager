362ec1cca435e498c2215124e1aaa50e2b8d9198
Michael Fryer <michael.d.fryer@gmail.com>
2017-06-29 23:12:14 -0600

    fixed the scollbar popping up in largescreen mode for menubar links

--- acm-manager-web-application/app/components/nav-bar.js
+++ acm-manager-web-application/app/components/nav-bar.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   smallScreen: false,
+  
+  didRender() {
+    this.set('smallScreen', parseInt(Ember.$(window).width()) < 1200);
+  }
 });
