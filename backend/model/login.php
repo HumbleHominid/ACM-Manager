3e50978d6ea367144b6d8c3531968e0925957d16
trevorlbrooks <trevor@trevorlbrooks.com>
2017-07-01 17:07:33 -0600

    Started refactor of server.php

--- backend/model/login.php
+++ backend/model/login.php
@@ -5,172 +5,177 @@ require_once 'jwt/src/ExpiredException.php';
 require_once 'jwt/src/SignatureInvalidException.php';
 require_once 'jwt/src/JWT.php';
 
-use \Firebase\JWT\JWT; 
+use \Firebase\JWT\JWT;
 
-define('ALGORITHM','HS512'); 
+define('ALGORITHM','HS512');
 
 class Login{
 
-  private $domain = 'katie.mtech.edu/~acmuser';
-  private $validatedUser = -1;
-  private $currToken = '';
+   private $domain = 'katie.mtech.edu/~acmuser';
+   private $validatedUser = -1;
+   private $currToken = '';
 
-  private $fName = '';
-  private $lName = '';
-  private $email = ''; 
-  private $user_type_id = '';
-  private $name = '';
-     
-
-  function attemptLogin($user, $pass){
-    include('dbStartup.php');
-    $query = 'SELECT * FROM Users
-      JOIN Passwords ON Users.password_id = Passwords.password_id
-      WHERE (password_timeout >= NOW() OR password_timeout IS NULL) 
-      AND email = :user;';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':user', $user);
-    $statement->execute();
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $statement->closeCursor();
-
-    if(COUNT($results) === 1){ 
-      if(password_verify($pass, $results[0]['password'])){
-        $validatedUser = $results[0]['user_id'];
-        
-        $typeId = $results[0]['user_type'];
-$userType = "SELECT * FROM User_Type WHERE user_type_id = :typeId";
-$statement = $db->prepare($userType);
-$statement->bindValue(':typeId', $typeId);
-$statement->execute();
-$typeResults = $statement->fetchAll(PDO::FETCH_ASSOC);
-$statement->closeCursor();
+   private $fName = '';
+   private $lName = '';
+   private $email = '';
+   private $user_type_id = '';
+   private $name = '';
 
 
-      $fName = $results[0]['fName'];
-      $lName = $results[0]['lName'];
-      $email = $results[0]['email']; 
-      $user_type_id = $typeId;
-      $name = $typeResults[0]['name'];  
-  
-        return TRUE; 
-  }
-}
-return FALSE;
-  }   
-
-  public function validateToken($user){
-    try {
-      include('key.php');
-      $secretKey = base64_decode($SECRET_KEY);  
-      $token = json_decode($user, true); 
-      $JWT = $token['jwt']; 
-      $DecodedDataArray = JWT::decode($JWT, $secretKey, array(ALGORITHM));
-      $decodedToken = json_decode($token, true);
-    
-      $fName = $decodedToken['data']['fName'];
-      $lName = $decodedToken['data']['lName'];
-      $email = $decodedToken['data']['email'];
-      $validatedUser = $decodedToken['data']['user_id']; 
-      $user_type_id = $decodedToken['data']['user_type']['user_type_id'];
-      $name = $decodedToken['data']['user_type']['name'];
-   
-      return true; 
-    } catch (Exception $e) {
-      return false; 
-    } 
-  }
-
-  public function createUser($user, $pass, $first, $last){
-    include('dbStartup.php');
-    $query = 'INSERT INTO Passwords(password,password_timeout) VALUES (:pass, NULL);';
-
-    $query2 =	'INSERT INTO Users(password_id, user_type, fName, lName, email) VALUES (:id, 1,:first, :last, :user);';
-
-    try{
-      $statement = $db->prepare($query);
-      $statement->bindValue(':pass', $pass); 
-      $statement->execute();  
-
-      $statement2 = $db->prepare($query2);
-      $statement2->bindValue(':user', $user);     
-      $statement2->bindValue(':first', $first);
-      $statement2->bindValue(':last', $last);
-      $statement2->bindValue(':id', $db->lastInsertId());
-      $statement2->execute();
-    }catch(Exception $e){
-      echo $e->getMessage();
-    }
-  }
-
-
-  public function getToken(){
-    if($validatedUser > -1){
+   function attemptLogin($user, $pass){
       include('dbStartup.php');
       $query = 'SELECT * FROM Users
-        WHERE user_id = :user;';
-$statement = $db->prepare($query);
-$statement->bindValue(':user', $validatedUser);
-$statement->execute();
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-$statement->closeCursor();
+      JOIN Passwords ON Users.password_id = Passwords.password_id
+      WHERE (password_timeout >= NOW() OR password_timeout IS NULL)
+      AND email = :user;';
+      $statement = $db->prepare($query);
+      $statement->bindValue(':user', $user);
+      $statement->execute();
+      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+      $statement->closeCursor();
 
-include('key.php');
+      if(COUNT($results) === 1){
+         if(password_verify($pass, $results[0]['password'])){
+            $this->validatedUser = $results[0]['user_id'];
 
-$typeId = $results[0]['user_type'];
-$userType = "SELECT * FROM User_Type WHERE user_type_id = :typeId";
-$statement = $db->prepare($userType);
-$statement->bindValue(':typeId', $typeId);
-$statement->execute();
-$typeResults = $statement->fetchAll(PDO::FETCH_ASSOC);
-$statement->closeCursor();
+            $typeId = $results[0]['user_type'];
+            $userType = "SELECT * FROM User_Type WHERE user_type_id = :typeId";
+            $statement = $db->prepare($userType);
+            $statement->bindValue(':typeId', $typeId);
+            $statement->execute();
+            $typeResults = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $statement->closeCursor();
 
-$tokenId    = base64_encode(mcrypt_create_iv(32));
 
-$data = array(
-  'data' => [
-    'fName' => $results[0]['fName'],
-    'lName' => $results[0]['lName'],
-    'email' => $results[0]['email'],
-    'user_id' => $results[0]['user_id'],
-    'user_type' => array(
-      'user_type_id' => $typeId,
-      'name' => $typeResults[0]['name'],
-      'description' => $typeResults[0]['description']
-    )
-  ],    
-  'jti' => $tokenId,
-  'iss' => $this->domain,
-  'aud' => $this->domain,
-  'iat' => time(),
-  'exp' => time() + 3600,
-  'nbf' => time() 
-);
+            $this->fName = $results[0]['fName'];
+            $this->lName = $results[0]['lName'];
+            $this->email = $results[0]['email'];
+            $this->user_type_id = $typeId;
+            $this->name = $typeResults[0]['name'];
 
-$secretKey = base64_decode($SECRET_KEY);
-$jwt = JWT::encode(
-  $data,
-  $secretKey,
-  ALGORITHM
-);
-$token = array(
-  'user' => [
-        'jwt' => $jwt,
-        'fName' => $results[0]['fName'],
-        'lName' => $results[0]['lName'],
-        'email' => $results[0]['email'], 
-        'user_id' => $results[0]['user_id'],
-        'user_type' => array(
-          'user_type_id' => $typeId,
-          'name' => $typeResults[0]['name'],
-          'description' => $typeResults[0]['description']
-        )
-      ]
-    );
-    return $token;
+            return TRUE;
+         }
+      }
+      return FALSE;
+   }
 
-    }
-  }
+   public function validateToken($inToken){
+      try {
+         include('key.php');
+         $secretKey = base64_decode($SECRET_KEY);
+         $token = json_decode($inToken, true);
+         $JWT = $token['jwt'];
+         $DecodedDataArray = JWT::decode($JWT, $secretKey, array(ALGORITHM));
+         $decodedToken = json_decode($token, true);
 
-}
-?>
+         $this->fName = $decodedToken['data']['fName'];
+         $this->lName = $decodedToken['data']['lName'];
+         $this->email = $decodedToken['data']['email'];
+         $this->validatedUser = $decodedToken['data']['user_id'];
+         $this->user_type_id = $decodedToken['data']['user_type']['user_type_id'];
+         $this->name = $decodedToken['data']['user_type']['name'];
+
+         return true;
+      } catch (Exception $e) {
+         return false;
+      }
+   }
+
+   public function createUser($user, $pass, $first, $last){
+      include('dbStartup.php');
+      $query = 'INSERT INTO Passwords(password,password_timeout) VALUES (:pass, NULL);';
+
+      $query2 = 'INSERT INTO Users(password_id, user_type, fName, lName, email) VALUES (:id, 1,:first, :last, :user);';
+
+      try{
+         $statement = $db->prepare($query);
+         $statement->bindValue(':pass', $pass);
+         $statement->execute();
+
+         $statement2 = $db->prepare($query2);
+         $statement2->bindValue(':user', $user);
+         $statement2->bindValue(':first', $first);
+         $statement2->bindValue(':last', $last);
+         $statement2->bindValue(':id', $db->lastInsertId());
+         $statement2->execute();
+
+         return attemptLogin($user, $pass);
+      }catch(Exception $e){
+         echo $e->getMessage();
+         die();
+      }
+   }
+
+
+   public function getToken(){
+      if($validatedUser > -1){
+         include('dbStartup.php');
+         $query = 'SELECT * FROM Users
+         WHERE user_id = :user;';
+         $statement = $db->prepare($query);
+         $statement->bindValue(':user', $validatedUser);
+         $statement->execute();
+         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+         $statement->closeCursor();
+
+         include('key.php');
+
+         $typeId = $results[0]['user_type'];
+         $userType = "SELECT * FROM User_Type WHERE user_type_id = :typeId";
+         $statement = $db->prepare($userType);
+         $statement->bindValue(':typeId', $typeId);
+         $statement->execute();
+         $typeResults = $statement->fetchAll(PDO::FETCH_ASSOC);
+         $statement->closeCursor();
+
+         $tokenId    = base64_encode(mcrypt_create_iv(32));
+
+         $data = array(
+            'data' => [
+               'fName' => $results[0]['fName'],
+               'lName' => $results[0]['lName'],
+               'email' => $results[0]['email'],
+               'user_id' => $results[0]['user_id'],
+               'user_type' => array(
+                  'user_type_id' => $typeId,
+                  'name' => $typeResults[0]['name'],
+                  'description' => $typeResults[0]['description']
+               )
+               ],
+               'jti' => $tokenId,
+               'iss' => $this->domain,
+               'aud' => $this->domain,
+               'iat' => time(),
+               'exp' => time() + 3600,
+               'nbf' => time()
+            );
+
+            $secretKey = base64_decode($SECRET_KEY);
+            $jwt = JWT::encode(
+               $data,
+               $secretKey,
+               ALGORITHM
+            );
+            $token = array(
+               'user' => [
+                  'jwt' => $jwt,
+                  'fName' => $results[0]['fName'],
+                  'lName' => $results[0]['lName'],
+                  'email' => $results[0]['email'],
+                  'user_id' => $results[0]['user_id'],
+                  'user_type' => array(
+                     'user_type_id' => $typeId,
+                     'name' => $typeResults[0]['name'],
+                     'description' => $typeResults[0]['description']
+                  )
+                  ]
+               );
+               return $token;
+
+            }else{
+               return NULL;
+            }
+         }
+
+      }
+      ?>
--- backend/server.php
+++ backend/server.php
@@ -6,262 +6,280 @@ error_reporting(E_ALL);
 
 class Server{
 
-  private $relAddress = '/~acmuser/backend/';
+   private $relAddress = '/~acmuser/backend/';
+   private $login;
+   private $data;
 
-  //Main handler method
-  public function serve(){
-    //Get the URL info for REST
-    $URI = str_replace($this->relAddress, '', $_SERVER['REQUEST_URI']);
-    
-    //Get the method of the request.
-    $method = $_SERVER['REQUEST_METHOD'];
+   //Main handler method
+   public function serve(){
+      //Get the URL info for REST
+      $URI = str_replace($this->relAddress, '', $_SERVER['REQUEST_URI']);
 
-    //Explode the trimmed URI   
-    $explArr = explode('/', $URI);
-    //Get the object and id of the request
-    $object = $explArr[0];
-    $id = '';
-    if(count($explArr) >= 2){
-      $id = $explArr[1];
-    }
-    
-    switch($object){
-      case 'events':
-        $this->events($method, $id); 
-        break;
-      case 'members':
-        $this->members($method, $id);
-        break;
-      case 'login':
-        $this->login($method, $id);
-        break;
-      case 'fees':
-        $this->fees($method, $id);
-        break;
-      case 'files':
-        $this->files($method, $id);
-        break;
-      case 'officers':
-        $this->officers($method, $id);
-        break;
-      case 'pass':
-        echo password_hash($id, PASSWORD_BCRYPT);
-      case 'testValidate':
-        include('model/login.php');
-        $login = new Login();
-        $login->validateToken();
-      case 'testInfoPath':
-        include('model/login.php');
-        $login = new Login;
+      //Refactoring out. Should always be post.
+      /*//Get the method of the request.
+      $method = $_SERVER['REQUEST_METHOD'];*/
 
-        // $data = json_decode(file_get_contents('php://input'), true);
-        //  $jwt = $data['jwt'];     
+      //Explode the trimmed URI
+      $explArr = explode('/', $URI);
+      //Get the object and id of the request
+      $object = $explArr[0];
+      $id = '';
+      if(count($explArr) >= 2){
+         $id = $explArr[1];
+      }
 
-        $arr = $login->validateToken(file_get_contents('php://input'));
-        var_dump($arr);
-        //echo $login->getCurrentUser($arr); 
-        //header('HTTP/1.1 200 OK');
-        break;
-      default:
-        echo $object;   
-        header('HTTP/1.1 400 Bad Request');
-        break;
-    }
+      $data = json_decode(file_get_contents('php://input'), true);
 
-  }
+      require_once('model/login.php');
+      $this->login = new Login;
+      if(!empty($data['token'])){
+         $login->validateToken($data['token']);
+      }
 
-  private function events($method, $id){
-    $item = NULL;
-    switch($method){
-      case 'GET': 
-        if(!isset($id) || strlen($id) === 0){
-          include('model/events.php');
-          $events = new Events; 
-          $list = $events->sidebar();
-          echo $this->response($list);
-        }        
-        break;
-      case 'PUT':
-        if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
-        }
-        break;
-      case 'POST':
+      switch($object){
+         case 'events':
+         $this->events($id);
+         break;
+         case 'members':
+         $this->members($id);
+         break;
+         case 'login':
+         $this->login($id);
+         break;
+         case 'fees':
+         $this->fees($id);
+         break;
+         case 'files':
+         $this->files($id);
+         break;
+         case 'officers':
+         $this->officers($id);
+         break;
+         case 'testValidate':
+         include('model/login.php');
+         $login = new Login();
+         $login->validateToken();
+         case 'testInfoPath':
+         include('model/login.php');
+         $login = new Login;
+
+         // $data = json_decode(file_get_contents('php://input'), true);
+         //  $jwt = $data['jwt'];
+
+         $arr = $login->validateToken(file_get_contents('php://input'));
+         var_dump($arr);
+         //echo $login->getCurrentUser($arr);
+         //header('HTTP/1.1 200 OK');
+         break;
+         default:
+         echo $object;
+         header('HTTP/1.1 400 Bad Request');
+         break;
+      }
+   }
+
+   private function events($id){
+      $item = NULL;
+      $task = $this->data['task'];
+      switch($task){
+         case 'GET':
+         if(!isset($id) || strlen($id) === 0){
+            include('model/events.php');
+            $events = new Events;
+            $list = $events->sidebar();
+            echo $this->response($list);
+         }
+         break;
+         case 'PUT':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
-
-        break;
-      case 'DELETE':
+         break;
+         case 'POST':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-    }
-    $this->response($item);
-  }
- 
-  private function members($method, $id){
-    switch($method){
-      case 'GET':
-        break;
-      case 'PUT':
-        if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
-        }
-        break;
-      case 'POST':
-        if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
-         }
-        break;
-      case 'DELETE':
+         break;
+         case 'DELETE':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-    } 
-  }
+         break;
+      }
+      $this->response($item);
+   }
 
-  private function login($method, $id){
-    switch($method){
-      case 'POST': 
-        
-        include('model/login.php');
-        $login = new Login;
-  	    $data = json_decode(file_get_contents('php://input'), true);
-        $user = $data['username'];
-      	$pass = $data['password']; 
-        
-        $result = $login->attemptLogin($user, $pass);
-        $json = $this->response($result);
-        if($json !== 'false'){
-          echo $json;
-        }else{
-          echo 'INVALID CREDENTIALS';
-        }
-        break; 
-      case 'PUT':
-      	include('model/login.php');
-      	$login = new Login;
-      	$data = json_decode(file_get_contents('php://input'), true);
-
-        $user = $data['username'];
-      	$pass = password_hash($data['password'], PASSWORD_BCRYPT); 
-      	$first = $data['first'];
-      	$last = $data['last'];
-      	$result = $login->createUser($user, $pass, $first, $last); 
-        header('HTTP/1.1 200 OK');
-        break;
-    } 
-  }
-
-  private function fees($method, $id){
-    switch($method){
-      case 'GET':
-        break;
-      case 'PUT':
+   private function members($id){
+      $task = $this->data['task'];
+      switch($task){
+         case 'GET':
+         break;
+         case 'PUT':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
-
-        break;
-      case 'POST':
+         break;
+         case 'POST':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
-
-        break;
-      case 'DELETE':
+         break;
+         case 'DELETE':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-    } 
-  }
-  
-  private function files($method, $id){
-    switch($method){
-      case 'GET':
-        break;
-      case 'PUT':
+         break;
+      }
+   }
+
+   private function login($id){
+      $task = $this->data['task'];
+      switch($task){
+         case 'ATTEMPT_LOGIN':
+
+         $user = $this->data['data']['username'];
+         $pass = $this->data['data']['password'];
+
+         $result = $this->login->attemptLogin($user, $pass);
+         if($result){
+            $json = $this->response(array());
+            echo $json;
+         }else{
+            echo 'INVALID CREDENTIALS';
+         }
+         break;
+         case 'CREATE':
+
+         $user = $this->data['data']['username'];
+         $pass = password_hash($this->data['data']['password'], PASSWORD_BCRYPT);
+         $first = $this->data['data']['first'];
+         $last = $this->data['data']['last'];
+         $result = $this->login->createUser($user, $pass, $first, $last);
+         if($result){
+            $json = $this->response(array());
+            echo $json;
+         }else{
+            echo 'Error creating account.';
+         }
+         header('HTTP/1.1 200 OK');
+         break;
+      }
+   }
+
+   private function fees($id){
+      $task = $this->data['task'];
+      switch($task){
+         case 'GET':
+         break;
+         case 'PUT':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-      case 'POST':
+         break;
+         case 'POST':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-      case 'DELETE':
+         break;
+         case 'DELETE':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-    } 
-  }
-  
-  private function officers($method, $id){ 
-    switch($method){
-      case 'GET':
-        break;
-      case 'PUT':
+         break;
+      }
+   }
+
+   private function files($id){
+      $task = $this->data['task'];
+      switch($task){
+         case 'GET':
+         break;
+         case 'PUT':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-      case 'POST':
+         break;
+         case 'POST':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-      case 'DELETE':
+         break;
+         case 'DELETE':
          if($id === ''){
-          header('HTTP/1.1 400 Bad Request');
-          exit();
+            header('HTTP/1.1 400 Bad Request');
+            exit();
          }
 
-        break;
-    } 
-  } 
-  
-  
-  //Encodes the JSON response. If it is not valid, sends error header and ends.
-  private function response($builtArr){
-    $json = json_encode($builtArr);
+         break;
+      }
+   }
 
-    if($json === FALSE){
-      header('HTTP/1.1 500 Internal Server Error');
-      exit();
-    }else{
-      header('Content-Type: application/json');
-      return $json;
-    }
-    return FALSE;
-  }
+   private function officers($id){
+      $task = $this->data['task'];
+      switch($task){
+         case 'GET':
+         break;
+         case 'PUT':
+         if($id === ''){
+            header('HTTP/1.1 400 Bad Request');
+            exit();
+         }
+
+         break;
+         case 'POST':
+         if($id === ''){
+            header('HTTP/1.1 400 Bad Request');
+            exit();
+         }
+
+         break;
+         case 'DELETE':
+         if($id === ''){
+            header('HTTP/1.1 400 Bad Request');
+            exit();
+         }
+
+         break;
+      }
+   }
+
+
+   //Encodes the JSON response. If it is not valid, sends error header and ends.
+   private function response($builtArr){
+      $token = $this->login->getToken();
+      if(!empty($token)){
+         array_push($builtArr, $token);
+      }
+      $json = json_encode($builtArr);
+
+      if($json === FALSE){
+         header('HTTP/1.1 500 Internal Server Error');
+         exit();
+      }else if(count($builtArr) > 0) {
+         header('Content-Type: application/json');
+         return $json;
+      }
+      return FALSE;
+   }
 }
 
 $server = new Server;
@@ -269,4 +287,3 @@ $server->serve();
 
 
 ?>
-
