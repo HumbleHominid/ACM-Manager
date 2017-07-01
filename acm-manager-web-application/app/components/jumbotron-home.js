879c36a49f17fcf005f867b0b7e0d6dbb5db3908
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-01 12:22:44 -0600

    jumbotron and sidebar noww adjusts using media queries instead of jquery

--- acm-manager-web-application/app/components/jumbotron-home.js
+++ acm-manager-web-application/app/components/jumbotron-home.js
@@ -4,14 +4,5 @@ export default Ember.Component.extend({
   classNames: [ "jumbotron" ],
   headerClass: "",
   paragraphClass: "",
-  title: "",
-  
-  didRender() {
-    if (parseInt(Ember.$(window).width()) > 1225) {
-      let el = Ember.$("#" + this.elementId);
-      let sidebar = Ember.$("#upcoming-events");
-      
-      el.css("marginRight", parseInt(sidebar.css("right")) + sidebar.outerWidth(true) + parseInt(el.css("marginLeft")));
-    }  
-  }
+  title: ""
 });
