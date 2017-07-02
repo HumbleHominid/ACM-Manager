0d59d4eb69325fa4e07bd7dc67c3b5673e7833e1
trevorlbrooks <trevor@trevorlbrooks.com>
2017-07-02 10:43:49 -0600

    Updated generation and population scripts for db to new standard.

--- acm_Database_Placeholder_Data.sql
+++ acm_Database_Placeholder_Data.sql
@@ -1,10 +1,48 @@
-INSERT INTO Passwords (password, pjsalt)
-  VALUES ('schuckles', 'catch em all');
-
 INSERT INTO User_Type (name, description)
-  VALUES ('pokemon', 'slaver');
+  VALUES ('Member', 'Club Member');
+  
+INSERT INTO User_Type (name, description)
+  VALUES ('Treasurer', 'Treasurer');
+  
+INSERT INTO User_Type (name, description)
+  VALUES ('Secretary', 'Secretary');
+  
+INSERT INTO User_Type (name, description)
+  VALUES ('Vice President', 'Vice President');
+  
+INSERT INTO User_Type (name, description)
+  VALUES ('President', 'President');
 
-INSERT INTO Users (password_id, user_type, fName, lName, email)
-  VALUES (1, 1, 'Shucklee', 'Norse', 'schuckles@pokemon.catch');
-
-
+INSERT INTO Fee_Type (name, description)
+	VALUES ('Dues', 'Yearly dues. $10');
+	
+INSERT INTO Fee_Type (name, description)
+	VALUES ('T-Shirt', 'Yearly T-shirt');
+	
+INSERT INTO Fee_Type (name, description)
+	VALUES ('Pizza', 'Money for pizza');
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Meeting', 'General club meetings for all members', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('LAN Party', 'LAN Party, games, and pizza', 0);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Programming Comp', 'Programming competitions', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Programming Prac', 'Practice for competitions', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Industry Trip', 'Going out and seeing companies', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Homecoming', 'Attendance for Homecoming events', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('Club Rush', 'Club Rush help', 1);
+	
+INSERT INTO Event_Type (name, description, defaultPoints)
+	VALUES ('ASMT Meeting', 'Attendance for an ASMT proposal meeting', 1);
+	
