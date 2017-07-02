3cdb4c427ff8b1d0317b38b5f492be8c3ca3236e
trevorlbrooks <trevor@trevorlbrooks.com>
2017-07-02 00:01:40 -0600

    Start of request api doc

---
+++ acm-manager-request-data.js
@@ -0,0 +1,33 @@
+//Account Creation
+{
+    "task": "CREATE_ACCOUNT",
+    "data": {
+        "username": "testuser@mtech.edu",
+        "password": "Password!1",
+                "first":  "My First Name",
+                 "last": "Last Name"
+    }
+}
+
+//Login
+{
+    "task": "ATTEMPT_LOGIN",
+    "data": {
+        "username": "testuser@mtech.edu",
+        "password": "Password!1"
+    }
+}
+
+//Error
+{
+"reason": "It borked because reasons",
+.....
+}
+
+//Logged in request
+{
+"token": "asf;ljaweo;iwepoiuwerlkvdslkn",
+/* Remaining Data*/
+}
+
+
