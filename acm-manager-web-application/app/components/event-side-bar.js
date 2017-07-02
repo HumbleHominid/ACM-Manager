849c3227da357c366daa6c2f7270f3e5fdf5c3f4
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-02 13:17:02 -0600

    You can filter events by time and type now. default is time. filtering by type still seperates them into past and future and displays them chronologically.

--- acm-manager-web-application/app/components/event-side-bar.js
+++ acm-manager-web-application/app/components/event-side-bar.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  past: null,
-  future: null,
+  data: { 
+    past: null,
+    future: null
+  },
+  filterTime: true,
+  filterType: false,
+  
   init() {
     this._super(...arguments);
     
-    this.set('past', Ember.copy(this.get('eventData.past')));
-    this.set('future', Ember.copy(this.get('eventData.future')));
+    this.set('data.past', Ember.copy(this.get('eventData.past')));
+    this.set('data.future', Ember.copy(this.get('eventData.future')));
   },
   didRender() {
     let el = this.$(".upcoming-events-localized");
@@ -21,6 +26,51 @@ export default Ember.Component.extend({
     this.$(".upcoming-events-list").css("max-height", eventsHeight - (panelBodyHeight + panelHeadingHeight));
   },
   actions: {
-    
+    filterTime() {
+      this.set('filterTime', true);
+      this.set('filterType', false);
+      
+      this.set('data.past', Ember.copy(this.get('eventData.past')));
+      this.set('data.future', Ember.copy(this.get('eventData.future')));
+    },
+    filterType() {
+      this.set('filterType', true);
+      this.set('filterTime', false);
+      
+      let data = { 
+        past: { },
+        future: { }
+      };
+      
+      this.get('eventData.past').forEach(function(event) {
+        if (event.eventType.event_type_id in data.past) {
+          data.past[event.eventType.event_type_id].events.push(event);
+        }
+        else {
+          data.past[event.eventType.event_type_id] = {
+            name: event.eventType.name,
+            events: [ ]
+          };
+          
+          data.past[event.eventType.event_type_id].events.push(event);
+        }
+      }, this);
+          
+      this.get('eventData.future').forEach(function(event) {
+        if (event.eventType.event_type_id in data.future) {
+          data.future[event.eventType.event_type_id].events.push(event);
+        }
+        else {
+          data.future[event.eventType.event_type_id] = {
+            name: event.eventType.name,
+            events: [ ]
+          };
+          
+          data.future[event.eventType.event_type_id].events.push(event);
+        }
+      }, this);
+      
+      this.set('data', data);
+    }
   }
 });
