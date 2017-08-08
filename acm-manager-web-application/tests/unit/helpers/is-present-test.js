9ab621659c8b44adeee76dded10a200cbda48606
Michael Fryer <michael.d.fryer@gmail.com>
2017-08-07 22:30:50 -0700

    template for adding an attendee is up

---
+++ acm-manager-web-application/tests/unit/helpers/is-present-test.js
@@ -0,0 +1,12 @@
+
+import { isPresent } from 'acm-manager-web-application/helpers/is-present';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | is present');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = isPresent([42]);
+  assert.ok(result);
+});
+
