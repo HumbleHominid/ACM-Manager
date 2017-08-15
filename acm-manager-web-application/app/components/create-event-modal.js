a9d872dfbf7f401d59f96c5cba9903798833d915
Michael Fryer <michael.d.fryer@gmail.com>
2017-08-15 16:30:02 -0600

    properly sorted the event type list in create-event-modal

--- acm-manager-web-application/app/components/create-event-modal.js
+++ acm-manager-web-application/app/components/create-event-modal.js
@@ -110,7 +110,19 @@ export default Ember.Component.extend({
         };
       });
       
-      data.sort();
+      data.sort(function(a, b) {
+        let nameA = a.text.toUpperCase();
+        let nameB = b.text.toUpperCase();
+        
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        return 0;
+      });
     }
     
     return data;
