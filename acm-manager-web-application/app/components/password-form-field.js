8a1f4d01e6e687bcc024f512bd045c00e990cca9
Michael Fryer <michael.d.fryer@gmail.com>
2017-08-30 16:35:07 -0600

    moving computers

--- acm-manager-web-application/app/components/password-form-field.js
+++ acm-manager-web-application/app/components/password-form-field.js
@@ -4,7 +4,7 @@ import FormOptions from '../mixins/form-options';
 export default Ember.Component.extend(FormOptions, {
   placeholder: "Password",
   formName: "password",
-  pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,}).*$",
-  help: "One lowercase, one uppercase, one number, one special character (!@#$), and 8 character minimum",
+  pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}).*$",
+  help: "One lowercase, one uppercase, one number, and 8 character minimum",
   label: "Password"
 });
--- acm-manager-web-application/app/services/members.js
+++ acm-manager-web-application/app/services/members.js
@@ -1,4 +1,74 @@
 import Ember from 'ember';
 
+const { inject: { service }, $ } = Ember;
+
 export default Ember.Service.extend({
+  _metadata: service('metadata'),
+  _notify: service('notify'),
+  session: service(),
+  currentUser: service(),
+  
+  _data: null,
+  
+  init() {
+    "use strict";
+    
+    this.clear();
+  },
+  load() {
+    "use strict";
+    
+    let metadata = this.get('_metadata');
+    
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: `${metadata.get('url')}members`,
+      data: JSON.stringify({
+        task: 'LIST_MEMBERS',
+        token: this.get('currentUser.token')
+      })
+    }).done((data) => {console.log(data)
+      this.set('_data', data.memberList);
+    }).fail(() => {
+      this.get('_notify').alert("Failed to pull member list.", {
+        radius: true,
+        closeAfter: 3 * 1000
+      });
+    });
+  },
+  search(searchString) {
+    "use strict";
+    
+    let string = searchString.toLowerCase();
+    let members = this.get('data');
+    
+    if (Ember.isNone(members) || !Ember.isArray(members)) {
+      return;
+    }
+  
+    let foundMembers = [ ];
+    
+    members.forEach(function(member) {
+      let name = member.fName + " " + member.lName;
+      
+      if (name.indexOf(string) !== -1) {
+        foundMembers.push(member);
+      }
+    });
+    
+    return foundMembers;
+  },
+  data: Ember.computed('_data', function() {
+    "use strict";
+    
+    let data = this.get('_data');
+    
+    return data ? data : null;
+  }),
+  clear() {
+    "use strict";
+    
+    this.set('_data', null);
+  }
 });
